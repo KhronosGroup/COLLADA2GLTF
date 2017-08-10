16888216d94a8579cba8320586f557b20b2ef501
Goneflash <fz.zhangfan@gmail.com>
2017-08-09 23:35:46 -0700

    Add script for processing models

---
+++ my_script.sh
@@ -0,0 +1,20 @@
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/Box/Box.dae -d -s -o ../../gltf-test/sampleModels/Box/glTF-Draco/Box.gltf
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/BoxWithoutIndices/BoxWithoutIndices.dae -d -s -o ../../gltf-test/sampleModels/BoxWithoutIndices/glTF-Draco/BoxWithoutIndices.gltf
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/BoxTextured/BoxTextured.dae -d -o ../../gltf-test/sampleModels/BoxTextured/glTF-Draco/BoxTextured.gltf
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/BoxSemantics/BoxSemantics.dae -d -o ../../gltf-test/sampleModels/BoxSemantics/glTF-Draco/BoxSemantics.gltf
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/Duck/Duck.dae -d -s -o ../../gltf-test/sampleModels/Duck/glTF-Draco/Duck.gltf
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/SmilingFace/SmilingFace.dae -d -s -o ../../gltf-test/sampleModels/SmilingFace/glTF-Draco/SmilingFace.gltf
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/2CylinderEngine/2CylinderEngine.dae -d -s -o ../../gltf-test/sampleModels/2CylinderEngine/glTF-Draco/2CylinderEngine.gltf
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/ReciprocatingSaw/ReciprocatingSaw.dae -d -s -o ../../gltf-test/sampleModels/ReciprocatingSaw/glTF-Draco/ReciprocatingSaw.gltf
+
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/Buggy/Buggy.dae -d -s -o ../../gltf-test/sampleModels/Buggy/glTF-Draco/Buggy.gltf
+
+
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/RiggedSimple/RiggedSimple.dae -d -s -o ../../gltf-test/sampleModels/RiggedSimple/glTF-Draco/RiggedSimple.gltf
+
+
+./COLLADA2GLTF-bin -i ../../glTF-Sample-Models/sourceModels/CesiumMan/CesiumMan.dae -d -s -o ../../gltf-test/sampleModels/CesiumMan/glTF-Draco/CesiumMan.gltf
+
+
+
+
