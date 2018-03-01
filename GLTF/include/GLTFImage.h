4e66a9f8a871603a31d6487349132b48c202059c
Peter Verswyvelen <bugfact@gmail.com>
2018-03-01 23:35:37 +0100

    Images are now removed from cache when destructed

--- GLTF/include/GLTFImage.h
+++ GLTF/include/GLTFImage.h
@@ -8,15 +8,23 @@ namespace GLTF {
 	public:
 		std::string uri;
 		unsigned char* data = NULL;
-		size_t byteLength;
+		size_t byteLength = 0;
 		std::string mimeType;
 		GLTF::BufferView* bufferView = NULL;
 
 		Image(std::string uri);
 		Image(std::string uri, unsigned char* data, size_t byteLength, std::string fileExtension);
+		virtual ~Image();
+
 		static GLTF::Image* load(path path);
 		std::pair<int, int> getDimensions();
 		virtual std::string typeName();
 		virtual void writeJSON(void* writer, GLTF::Options* options);
+
+	private:
+		const std::string cacheKey;
+
+		Image(std::string uri, std::string cacheKey);
+		Image(std::string uri, std::string cacheKey, unsigned char* data, size_t byteLength, std::string fileExtension);
 	};
 }
