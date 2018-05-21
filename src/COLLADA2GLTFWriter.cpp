c596b76ff9f80f0f914a1206698e041da3f03443
Akul Agrawal <akulagrawal@rediffmail.com>
2018-05-21 13:15:04 +0530

    Code properly indented

--- src/COLLADA2GLTFWriter.cpp
+++ src/COLLADA2GLTFWriter.cpp
@@ -1621,17 +1621,17 @@ bool COLLADA2GLTF::Writer::writeController(const COLLADAFW::Controller* controll
 		int numberOfComponents = GLTF::Accessor::getNumberOfComponents(type);
 
 		for (size_t i = 0; i < weights.size(); i++) {
-        	float weightSum = 0;
-        	float* weight = weights[i];
-        	for (size_t j = 0; j < numberOfComponents; j++) {
-            	weightSum = weightSum + std::abs(weight[j]);
-        	}
-        	if (weightSum > 0) {
-            	for (size_t j = 0; j < numberOfComponents; j++) {
-                	weight[j] = weight[j] / weightSum;
-            	}
-        	}
-    	}
+        		float weightSum = 0;
+        		float* weight = weights[i];
+        		for (size_t j = 0; j < numberOfComponents; j++) {
+            			weightSum = weightSum + std::abs(weight[j]);
+        		}
+        		if (weightSum > 0) {
+            			for (size_t j = 0; j < numberOfComponents; j++) {
+                			weight[j] = weight[j] / weightSum;
+            			}
+        		}
+    		}
 
 		COLLADAFW::UniqueId meshId = skinController->getSource();
 		GLTF::Mesh* mesh = _meshInstances[meshId];
